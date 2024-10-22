d9d1ecc3f9bc045ce5c6d92f976ee32cb0c55851
root <root@LAPTOP-GL03N44C>
2024-10-22 11:44:03 +0300

    Initial commit

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/eng')
-def eng():
-    return render_template('eng.html')
 
 # Galvenais izpildes bloks
 if __name__ == '__main__':
